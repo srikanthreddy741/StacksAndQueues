30e2b327465524cf5563583bdd72bc913ee3cfef
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-27 23:31:54 +0530

    Added Create Stack

---
+++ StackAndQueues/StackAndQueues/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackAndQueues;
+
+namespace StacksAndQueues
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Pushing values to Stack");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);//1st in and last in stack
+            linkedListStack.push(30);
+            linkedListStack.push(56);//last in and on top of stack(UC1 requirement)
+            linkedListStack.Display();
+        }
+    }
+}
