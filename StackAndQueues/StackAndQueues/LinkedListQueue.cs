2bea39e74d958470459d02a71f7dd106cfb21ad7
srikanthreddy741 <bandarisrikanth741@gmail.com>
2022-07-27 23:43:47 +0530

    Create Queue

---
+++ StackAndQueues/StackAndQueues/LinkedListQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueues
+{
+    internal class LinkedListQueue
+    {
+
+        Node head = null;
+        internal void Enqueue(int data) //use of Enqueue for adding elements in Queue like we used Push in stack 
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+        internal void Display() // method for displaying 
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+
+        }
+    }
+}
--- StackAndQueues/StackAndQueues/Program.cs
+++ StackAndQueues/StackAndQueues/Program.cs
@@ -6,19 +6,26 @@ namespace StacksAndQueues
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Pushing values to Stack");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.push(70);//1st in and last in stack
-            linkedListStack.push(30);
-            linkedListStack.push(56);//last in and on top of stack(UC1 requirement)
-            linkedListStack.Display();
+            //Console.WriteLine("Pushing values to Stack");
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.push(70);//1st in and last in stack
+            //linkedListStack.push(30);
+            //linkedListStack.push(56);//last in and on top of stack(UC1 requirement)
+            //linkedListStack.Display();
 
 
-            Console.WriteLine("Using Peek and Pop Till Stack is Empty");
-            linkedListStack.Peek();//shows top element of stack
-            linkedListStack.Pop();//deletes top element of stack
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
+            //Console.WriteLine("Using Peek and Pop Till Stack is Empty");
+            //linkedListStack.Peek();//shows top element of stack
+            //linkedListStack.Pop();//deletes top element of stack
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+
+            LinkedListQueue linkedListQueues = new LinkedListQueue();
+            linkedListQueues.Enqueue(56);//1ST in 1st out
+            linkedListQueues.Enqueue(30);
+            linkedListQueues.Enqueue(70);
+            linkedListQueues.Display();
+
         }
     }
 }
